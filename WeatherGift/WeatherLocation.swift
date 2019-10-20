c9838dbfbf8932336deefed90b1d98e52b12832b
Sandeep Singh <singhsz@bc.edu>
2019-10-19 23:12:28 -0400

    7.10 Complete

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Sandeep Singh on 10/19/19.
+//  Copyright © 2019 Sandeep Singh. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}
