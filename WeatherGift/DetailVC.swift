af3ff026e58101d9bb097a061998dfc82355ee6a
Sandeep Singh <singhsz@bc.edu>
2019-10-20 13:31:11 -0400

    7.14 Complete

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -24,9 +24,7 @@ class DetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        locationLabel.text = locationsArray[currentPage].name
-        dateLabel.text = locationsArray[currentPage].coordinates
+        updateUserInterface()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -34,6 +32,11 @@ class DetailVC: UIViewController {
         if currentPage == 0 {
             getLocation()
         }
+    }
+    
+    func updateUserInterface() {
+    locationLabel.text = locationsArray[currentPage].name
+    dateLabel.text = locationsArray[currentPage].coordinates
     }
 
 }
@@ -66,11 +69,24 @@ extension DetailVC: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let geoCoder = CLGeocoder()
+        var place = ""
         currentLocation = locations.last
         let currentLatitude = currentLocation.coordinate.latitude
         let currentLongitude = currentLocation.coordinate.longitude
         let currentCoordinates = "\(currentLatitude),\(currentLongitude)"
         dateLabel.text = currentCoordinates
+        geoCoder.reverseGeocodeLocation(currentLocation, completionHandler: {placemarks, error in
+            if placemarks != nil {
+                let placemark = placemarks?.last
+                place = (placemark?.name)!
+            } else {
+                print("Error retrieving place. Error code: \(error!)")
+            }
+            self.locationsArray[0].name = place
+            self.locationsArray[0].coordinates = currentCoordinates
+            self.updateUserInterface()
+        })
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
