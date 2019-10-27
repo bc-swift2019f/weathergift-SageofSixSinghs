a8003c880736aaed200d8cbb2e31f068bf7d7222
Sandeep Singh <singhsz@bc.edu>
2019-10-27 19:59:19 -0400

    7.20 Complete

---
+++ WeatherGift/DayWeatherCell.swift
@@ -0,0 +1,22 @@
+//
+//  DayWeatherCell.swift
+//  WeatherGift
+//
+//  Created by Sandeep Singh on 10/27/19.
+//  Copyright © 2019 Sandeep Singh. All rights reserved.
+//
+
+import UIKit
+
+class DayWeatherCell: UITableViewCell {
+    @IBOutlet weak var dayCellIcon: UIImageView!
+    @IBOutlet weak var dayCellWeekday: UILabel!
+    @IBOutlet weak var dayCellMaxTemp: UILabel!
+    @IBOutlet weak var dayCellMinTemp: UILabel!
+    @IBOutlet weak var dayCellSummary: UITextView!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
